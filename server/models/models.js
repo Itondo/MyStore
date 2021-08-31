05ad7b0f5b8ff935183847dfc3a85af0749e3160
alexeykolmyk@gmail.com <alexeykolmyk@gmail.com>
2021-08-31 14:13:26 +0300

    create Database and connect table

--- server/models/models.js
+++ server/models/models.js
@@ -2,22 +2,22 @@ const requelize = require('../db')
 const {DataTypes} = require ('sequelize')
 
 
-const User = sequelize.define('user',{
+const User = requelize.define('user',{
     id: {type:DataTypes.INTEGER,primaryKey:true, autoIncrement: true},
     email:{type:DataTypes.STRING,unique: true,},
     password:{type:DataTypes.STRING},
     role:{type:DataTypes.STRING,unique: true,defaultValue: "USER"},
 
 })
-const Basket = sequelize.define('basket', {
+const Basket = requelize.define('basket', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 
 })
-const BasketDevice = sequelize.define('basket_device', {
+const BasketDevice = requelize.define('basket_device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 
 })
-const Device = sequelize.define('device', {
+const Device = requelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name:{type:DataTypes.STRING,unique:true,allowNull:false},
     price:{type:DataTypes.INTEGER,allowNull:false},
@@ -25,20 +25,66 @@ const Device = sequelize.define('device', {
     img:{type:DataTypes.STRING,defaultValue:0},
 
 })
-const Type = sequelize.define('type', {
+const Type = requelize.define('type', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     Name: {type: DataTypes.STRING, unique:true,allowNull:false},
 })
-const Brand = sequelize.define('brand', {
+const Brand = requelize.define('brand', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     Name: {type: DataTypes.STRING, unique: true, allowNull: false},
 })
-const Rating = sequelize.define('rating', {
+const Rating = requelize.define('rating', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     rate: {type: DataTypes.INTEGER, allowNull: false},
 })
-const DeviceInfo = sequelize.define('device_info', {
+const DeviceInfo = requelize.define('device_info', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING,allowNull: false},
     description: {type:DataTypes.STRING,allowNull:false},
 })
+
+const TypeBrand = requelize.define('type_brand',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+/**Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)*/
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through:TypeBrand })
+Brand.belongsToMany(Type,{through:TypeBrand })
+
+module.exports =
+{
+    User,
+        Basket,
+        BasketDevice,
+        Device,
+        Type,
+        Brand,
+        Rating,
+        TypeBrand,
+        DeviceInfo
+}
+
