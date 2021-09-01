0c6b1fd2dcb7619f3024f05222a035583459f483
alexeykolmyk@gmail.com <alexeykolmyk@gmail.com>
2021-09-01 18:48:42 +0300

    Add routes

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT  || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.get('/',(req,res,)=>{
-    res.status(200).json({message: 'WORKING!!!'})
-})
+
     const start = async () => {
         try {
             await sequelize.authenticate()
---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const router = new Router()
+
+router.use('/use',)
+router.use('/type',)
+router.use('/brand',)
+router.use('/device',)
+
+
+module.exports = router
